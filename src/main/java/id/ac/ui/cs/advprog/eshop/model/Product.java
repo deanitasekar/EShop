7689fc0538472bf1f2695dc293c7f5635717409f
deanitasekar <deanitasekar@gmail.com>
2025-02-12 21:37:52 +0700

    edit Product (add product id)

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
@@ -3,9 +3,19 @@ package id.ac.ui.cs.advprog.eshop.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.UUID;
+
 @Getter @Setter
 public class Product {
     private String productId;
     private String productName;
     private int productQuantity;
+
+    private static String generateId() {
+        return UUID.randomUUID().toString();
+    }
+
+    public Product() {
+        productId = generateId();
+    }
 }
