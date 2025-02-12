0dd6aed818385f3b7834d2bb3c165c64fc97a6eb
deanitasekar <deanitasekar@gmail.com>
2025-02-12 23:14:22 +0700

    edit ProductController (add Delete functionality)

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -48,4 +48,10 @@ public class ProductController {
         service.edit(productId, selectedProduct);
         return "redirect:../list";
     }
+
+    @PostMapping("/delete/{productId}")
+    public String deleteProductPost(Model model, @PathVariable("productId") String productId) {
+        service.delete(productId);
+        return "redirect:/product/list";
+    }
 }
