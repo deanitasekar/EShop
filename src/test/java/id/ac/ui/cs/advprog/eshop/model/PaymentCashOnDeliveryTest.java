8c1e61c0db62d540f0fd891b3e67ca1c3ff4f214
deanitasekar <deanitasekar@gmail.com>
2025-03-07 19:12:40 +0700

    [REFACTOR] Fix PaymentCashOnDeliveryTest

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentCashOnDeliveryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentCashOnDeliveryTest.java
@@ -3,7 +3,10 @@ package id.ac.ui.cs.advprog.eshop.model;
 import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import static org.junit.jupiter.api.Assertions.*;
 
