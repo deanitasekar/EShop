74541e5808976fa1af0d86361f9941caaa095c8e
deanitasekar <deanitasekar@gmail.com>
2025-03-07 18:54:12 +0700

    [GREEN] Implement PaymentVoucherCode class

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/PaymentVoucherCode.java
@@ -0,0 +1,16 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import id.ac.ui.cs.advprog.eshop.enums.PaymentMethod;
+
+import java.util.Map;
+
+public class PaymentVoucherCode extends Payment {
+    public PaymentVoucherCode(String id, Order order, Map<String, String> paymentData) {
+        super(id, PaymentMethod.VOUCHER_CODE.getValue(), order, paymentData);
+    }
+
+    public PaymentVoucherCode(String id, Order order, Map<String, String> paymentData, String status) {
+        super(id, PaymentMethod.VOUCHER_CODE.getValue(), order, paymentData, status
+        );
+    }
+}
