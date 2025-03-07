b760da8207f3324bbd5a467bafc38eae57a6d194
deanitasekar <deanitasekar@gmail.com>
2025-03-07 18:52:58 +0700

    [RED] Add PaymentVoucherCode skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/PaymentVoucherCodeTest.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import java.util.Map;
+
+public class PaymentVoucherCode extends Payment {
+    public PaymentVoucherCode(String id, String method, Map<String, String> paymentData) {
+        super(id, method, paymentData);
+    }
+
+    public PaymentVoucherCode(String id, String method, Map<String, String> paymentData, String status) {
+        super(id, method, paymentData, status);
+    }
+}
